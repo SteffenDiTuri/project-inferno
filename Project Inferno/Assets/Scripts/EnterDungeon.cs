efe42f0858c1bd99b202ad7ef365edbf8d6f49f1
SteffenDiTuri <136519752+SteffenDiTuri@users.noreply.github.com>
2024-06-28 17:29:07 +0200

    Added Starter Room Dungeon Prefab

---
+++ Project Inferno/Assets/Scripts/EnterDungeon.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterDungeon : MonoBehaviour
+{
+    public GameObject Dungeon;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
