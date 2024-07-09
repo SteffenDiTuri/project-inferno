939a75f65b92624453d584af2c9b27de3eff396b
SteffenDiTuri <136519752+SteffenDiTuri@users.noreply.github.com>
2024-07-09 16:59:47 +0200

    logs removed

--- Project Inferno/Assets/Scripts/CrusadeButtonsBehaviour.cs
+++ Project Inferno/Assets/Scripts/CrusadeButtonsBehaviour.cs
@@ -6,13 +6,11 @@ using UnityEngine.SceneManagement;
 public class CrusadeButtonsBehaviour : MonoBehaviour
 {
     public void StartCrusadeButton(){
-        Debug.Log("You Successfully Started Another Crusade!");
 
         SceneManager.LoadScene("DungeonStartScene");
     }
 
     public void ReturnToHomeBaseButton(){
-        Debug.Log("You Successfully Went To Home Base!");
 
         SceneManager.LoadScene("HomeBase");
     }
