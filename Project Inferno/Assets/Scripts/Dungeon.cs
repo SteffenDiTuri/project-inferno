aab9f07d32356aa95c19b472fa6a6e9101d3d717
Wout Tessens <wtessens@gmail.com>
2024-07-09 14:47:42 +0200

    player prefab

--- Project Inferno/Assets/Scripts/Dungeon.cs
+++ Project Inferno/Assets/Scripts/Dungeon.cs
@@ -25,7 +25,7 @@ public class Dungeon : MonoBehaviour
         printTotalRooms();
         replaceBadDoors();
         printTotalRooms();
-        //addSecondaryRooms();
+        addSecondaryRooms();
         printTotalRooms();
     }
 
