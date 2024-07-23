408c019cef56776b4a823fd4bc70464edf5b33ac
Wout Tessens <wtessens@gmail.com>
2024-07-23 15:07:44 +0200

    start

---
+++ Project Inferno/Assets/Scripts/WeaponUpgrade.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponUpgrade : MonoBehaviour
+{
+    public int goldenSpoons;
+    public int redCoins;
+    public int obsidian;
+    public int coal;
+    public int metal;
+    public bool unlocked;
+}
